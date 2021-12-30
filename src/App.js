cea3a2c1e610444928821765be6ef6e080dc9b28
vuoso-l <vuosolucas@gmail.com>
2021-12-30 15:58:52 -0300

    Upload files and add gh-pages

--- src/App.js
+++ src/App.js
@@ -1,22 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Componente from "./components/Componente";
+import Propiedades from "./components/Propiedades";
+import Estado from "./components/Estado";
+import RenderizadoCondicional from "./components/RenderizadoCondicional";
+import RenderizadoElementos from "./components/RenderizadoElementos";
+import { EventosES6, EventosES7, MasSobreEventos } from "./components/Eventos";
+import ComunicacionComponentes from "./components/ComunicacionComponentes";
+import CicloVida from "./components/CicloVida";
+import AjaxApis from "./components/AjaxApis";
+import ContadorHooks from "./components/ContadorHooks";
+import ScrollHooks from "./components/ScrollHooks";
+import RelojHooks from "./components/RelojHooks";
+import AjaxHooks from "./components/AjaxHooks";
+import HooksPersonalizados from "./components/HooksPersonalizados";
+import Referencias from "./components/Referencias";
+import Formularios from "./components/Formularios";
+import Estilos from "./components/Estilos";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <section>
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </section>
+        <section>
+          <Estilos />
+          <Formularios />
+          <Referencias />
+          <HooksPersonalizados />
+          <AjaxHooks />
+          <RelojHooks />
+          <ScrollHooks />
+          <ContadorHooks titulo="Seguidores" />
+          <AjaxApis />
+          <CicloVida />
+          <ComunicacionComponentes />
+          <MasSobreEventos />
+          <EventosES7 />
+          <EventosES6 />
+          <RenderizadoElementos />
+          <RenderizadoCondicional />
+          <Estado />
+          <Propiedades
+            cadena="Esto es una cadena de texto"
+            numero={19}
+            booleano={true}
+            arreglo={[1, 2, 3]}
+            objeto={{ nombre: "Jon", correo: "jonmircha@gmail.com" }}
+            funcion={(num) => num * num}
+            elementoReact={<i>Esto es un elemento React</i>}
+            componenteReact={
+              <Componente msg="Soy un componente pasado como Prop" />
+            }
+          />
+          <Componente msg="Hola soy un Componente Funcional Expresado desde una prop" />
+        </section>
       </header>
     </div>
   );
